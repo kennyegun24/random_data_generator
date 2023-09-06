e6cf911a6987d2bdab467e9ab9961d2c1e57ab36
Kenny Egun <kennyegun240@gmail.com>
2023-09-06 13:06:44 +0100

    mock data to be used

---
+++ src/data/words.js
@@ -0,0 +1,5 @@
+export const article = ["the"];
+export const verbs = ["fight", "love", "buy", "destroy"];
+export const nouns = ["ken", "donald", "dog"];
+export const conjuction = ["and", "but", "in"];
+export const pronoun = ["her", "he", "him", "i"];
