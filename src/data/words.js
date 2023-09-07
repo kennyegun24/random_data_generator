d1e046d0618fcdbf2bdd73bc7e912069f84b3ba1
Kenny Egun <kennyegun240@gmail.com>
2023-09-07 19:19:18 +0100

    renamed files

--- src/data/words.js
+++
@@ -1,5 +0,0 @@
-export const article = ["the"];
-export const verbs = ["fight", "love", "buy", "destroy"];
-export const nouns = ["ken", "donald", "dog"];
-export const conjuction = ["and", "but", "in"];
-export const pronoun = ["her", "he", "him", "i"];
