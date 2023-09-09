2cfb823e68479d91a79f2a8ea46d42d60212b94b
Kenny Egun <kennyegun240@gmail.com>
2023-09-09 15:19:00 +0100

    Excel preview components

---
+++ src/component/ExcelPreview.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ExcelTable from "./subComponents/ExcelTable";
+import ExcelRaw from "./subComponents/ExcelRaw";
+
+const ExcelPreview = ({ arr }) => {
+  const [tab, setTab] = useState(1);
+  return (
+    <div className="width100">
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "0.5rem",
+          width: "100%",
+          height: "6%",
+        }}
+      >
+        <button className="button" onClick={() => setTab(1)} type="button">
+          Table
+        </button>
+        <button className="button" onClick={() => setTab(2)} type="button">
+          Raw
+        </button>
+      </header>
+      <div className="excelContainer">
+        {tab === 1 ? <ExcelTable arr={arr} /> : ""}
+        {tab === 2 ? <ExcelRaw arr={arr} /> : ""}
+      </div>
+    </div>
+  );
+};
+
+export default ExcelPreview;
